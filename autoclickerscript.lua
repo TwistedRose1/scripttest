bd42dca56b5bc23ade7e7052965c93d9a84fef36
TwistedRose1 <110665018+TwistedRose1@users.noreply.github.com>
2022-08-06 22:39:52 +0300

    Add files via upload

---
+++ autoclickerscript.lua
@@ -0,0 +1,192 @@
+getgenv().autoTap = false
+getgenv().autoRebirth = false
+getgenv().autoUpgrade = false
+getgenv().buyEgg = false
+
+local remotePath = game:GetService("ReplicatedStorage").Aero.AeroRemoteServices
+
+
+
+local clickMod = require(game:GetService("Players").YumutikTv.PlayerScripts.Aero.Controllers.UI.Click)
+clickMod:Click()
+
+
+
+function buyGamepass()
+    local gamepassMod = require(game:GetService("ReplicatedStorage").Aero.Shared.Gamepasses)
+    gamepassMod.HasPassOtherwisePrompt = function() return true end
+end
+
+function doTap()
+    spawn(function()
+        while autoTap == true do
+            clickMod:Click()
+            wait()
+        end
+    end)
+end
+
+
+
+function rebirth(rebirthAmount)
+    spawn(function()
+        while autoRebirth do
+            local args = {[1] = rebirthAmount} 
+            remotePath.RebirthService.BuyRebirths:FireServer(unpack(args))
+            wait()
+        end 
+    end)
+end
+
+
+
+function upgrader()
+    spawn(function ()
+        while autoUpgrade do
+          local args = {[1] = "ClickMultiply"}  
+          remotePath.UpgradeService.BuyUpgrade:FireServer(unpack(args))
+          wait()
+        end
+    end)
+end
+
+
+function buyEgg()
+    spawn(function()
+        while wait() do
+            if not getgenv.buyEgg then break end   
+            remotePath.EggService.Purchase:FireServer(selectedEgg)
+        end
+    end)
+end
+
+
+function getCurrentPlayerPOS()
+    local plyr = game.Players.LocalPlayer
+    if plyr.Character then
+        return plyr.Character.HumanoidRootPart.Position
+    end    
+        return false
+end
+
+function teleportTO(placeCFrame)
+    local plyr = game.Players.LocalPlayer
+    if plyr.Character then
+        plyr.Character.HumanoidRootPart.CFrame = placeCFrame
+    end    
+end
+
+function teleportWorld(world)
+    if game:GetService("Workspace").Worlds:FindFirstChild(world) then
+        teleportTO(game:GetService("Workspace").Worlds[world].Teleport.CFrame)
+    end
+end
+
+local library = loadstring(game:HttpGet(("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3")))()
+
+local w = library:CreateWindow("Clicker Madness") -- Creates the window
+
+local b = w:CreateFolder("AutoTap")
+
+local c = w:CreateFolder("Upgrader") -- Creates the folder(U will put here your buttons,etc)
+
+local d = w:CreateFolder("Teleport")
+
+local e = w:CreateFolder("Egg")
+
+local f = w:CreateFolder("Gamepasses")
+
+b:DestroyGui()
+
+local selectedRebirth
+b:Dropdown("Rebirth Amount",{"1","10","100","1000"},true,function(value)
+    selectedRebirth = value
+    print(value)
+end)
+
+
+b:Toggle("Auto Farm",function(bool)
+    getgenv().autoTap = bool
+    getgenv().autoRebirth = bool
+    print("Auto farm is now:", bool)
+    if bool then
+        doTap()
+        rebirth(selectedRebirth)
+    end
+end)
+
+c:Toggle("Auto Upgrade",function(bool)
+    getgenv().autoMultiply = bool
+    print("Auto upgrader is now:", bool)
+    if bool then
+        upgrader()
+    end
+end)
+
+
+local selectedWorld
+d:Dropdown("Choose World",{"Lava","Desert","Dominus","Fire"},true,function(value)
+    selectedWorld = value
+    print(value)
+end)
+
+d:Button("Teleport",function()
+    if selectedWorld then
+        teleportWorld(selectedWorld) 
+    end
+end)
+
+local selectedEgg
+e:Dropdown("Choose Egg",{"basic"},true,function(value)
+    selectedEgg = value
+    print(value)
+end)
+
+e:Toggle("Buy Egg",function(bool)
+    getgenv().buyEgg = bool
+    print("Auto Egg is now:", bool)
+    if bool then
+        buyEgg(selectedEgg)
+    end
+end)
+
+f:Button("Buy Gamepasses",function()
+    buyGamepass()
+end)
+
+--[[
+    b:Label("Pretty Useless NGL",{
+    TextSize = 25; -- Self Explaining
+    TextColor = Color3.fromRGB(201, 4, 4); -- Self Explaining
+    BgColor = Color3.fromRGB(51, 50, 49); -- Self Explaining
+    
+}) 
+
+b:Button("Button",function()
+    print("Elym Winning")
+end)
+
+b:Slider("Slider",{
+    min = 10; -- min value of the slider
+    max = 50; -- max value of the slider
+    precise = true; -- max 2 decimals
+},function(value)
+    print(value)
+end)
+
+b:Dropdown("Dropdown",{"A","B","C"},true,function(mob) --true/false, replaces the current title "Dropdown" with the option that t
+    print(mob)
+end)
+
+b:Bind("Bind",Enum.KeyCode.C,function() --Default bind
+    print("Yes")
+end)
+
+b:ColorPicker("ColorPicker",Color3.fromRGB(255,0,0),function(color) --Default color
+    print(color)
+end)
+
+b:Box("Box","number",function(value) -- "number" or "string"
+    print(value)
+end)
+]]
